972522613ca0787c8c62b24113a75e3cf40b1015
wangziqing <eininst@qq.com>
2017-07-04 14:19:58 +0800

    spring boot starter SessionFilter change to OncePerRequestFilter

--- onepiecex-session-share-spring-boot-starter/src/main/java/com/github/onepiecex/session/share/spring/boot/starter/SessionFilter.java
+++ onepiecex-session-share-spring-boot-starter/src/main/java/com/github/onepiecex/session/share/spring/boot/starter/SessionFilter.java
@@ -3,13 +3,17 @@ package com.github.onepiecex.session.share.spring.boot.starter;
 
 import com.github.onepiecex.session.share.core.wrapper.SessionShareRequestWrapper;
 import com.github.onepiecex.session.share.core.wrapper.SessionShareResponseWrapper;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 /**
  * Created by wangziqing on 17/6/28.
  */
-public class SessionFilter implements Filter {
+public class SessionFilter extends OncePerRequestFilter {
 
     private final SpringSessionShareConfig springSessionConfig;
 
@@ -18,19 +22,9 @@ public class SessionFilter implements Filter {
     }
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        SessionShareRequestWrapper requestWrapper = new SessionShareRequestWrapper(request,springSessionConfig);
-        SessionShareResponseWrapper responseWrapper = new SessionShareResponseWrapper(response,requestWrapper);
-        chain.doFilter(requestWrapper,responseWrapper);
-    }
-
-    @Override
-    public void destroy() {
-
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        SessionShareRequestWrapper sessionShareRequestWrapper = new SessionShareRequestWrapper(httpServletRequest,springSessionConfig);
+        SessionShareResponseWrapper sessionShareResponseWrapper = new SessionShareResponseWrapper(httpServletResponse,sessionShareRequestWrapper);
+        filterChain.doFilter(sessionShareRequestWrapper,sessionShareResponseWrapper);
     }
 }
